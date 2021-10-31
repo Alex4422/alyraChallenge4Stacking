d274ad0e9a77dd7b8994f8bb84cb7419f0e4cc20
Alex4422 <alexandregautier82@gmail.com>
2021-10-31 13:29:12 +0100

    bonus: implementation of deposit & issueTokens for Staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -5,9 +5,9 @@ import './StakeCoin.sol';
 
 contract Staking {
 
-    string public name = 'Stacking';
+    string public name = 'Staking';
     address public owner;
-    StackCoin public stackCoin;
+    StakeCoin public stakeCoin;
 
     //the different participants for our stacking contract
     address[] public stakers;
@@ -18,39 +18,54 @@ contract Staking {
     //this stacker (= address) has stacked?
     mapping(address => bool) public hasStaked;
 
-    //this stacker (= address) is stacked?
-    mapping(address => bool) public isStaked;
+    //this stacker (= address) is staking?
+    mapping(address => bool) public isStaking;
 
     /**
      * @dev Sets the value for {stackCoin}.
      */
-    constructor(StackCoin _stackCoin) public {
-        stackCoin = _stackCoin;
+    constructor(StakeCoin _stakeCoin) public {
+        stakeCoin = _stakeCoin;
     }
 
     /**
      * title depositTokens
-     * @author Alex
      * @notice allows the stakers to put money in this contract
      * @dev I use transferFrom from OpenZeppelin
-     * @param _amount
+     * @param _amount the sum of token ERC20 to stake
      */
     function depositTokens(uint _amount) public {
 
-        //require staking
+        //require staking amount to be greater than 0
+        require(_amount > 0, 'amount cannot be 0');
 
         //transfer stackCoin tokens to this contract address for this stakingBalance
-        stackCoin.transferFrom(msg.sender, address(this), _amount);
+        stakeCoin.transferFrom(msg.sender, address(this), _amount);
 
         //update staking balance
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
-        if(!hasStaked) {
-            stakers.push[msg.sender];
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
         }
 
         //update staking balance
-        isStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
         hasStaked[msg.sender] = true;
     }
+
+    function issueTokens() public {
+        //require the owner to issue tokens only
+        require(msg.sender == owner, 'caller must be the owner');
+
+        for (uint i=0; i<stakers.length; i++){
+            address recipient = stakers[i];
+            uint balance = stakingBalance(recipient);
+            if(balance > 0) {
+                rwd.transfer(recipient,balance);
+            }
+        }
+    }
+
 }
+
