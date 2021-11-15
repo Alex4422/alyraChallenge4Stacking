50202e6c9e42743c342528c0548c2aed1818c028
Alex <alexandregautier82@gmail.com>
2021-11-15 16:00:20 +0100

    modif of functions in staking.sol & begin of the front & begin of test

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.9;
 import './StakeCoin.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /**
     @title Staking
@@ -21,18 +22,59 @@ contract Staking is Ownable {
     /**
      * @notice The stakes for each stakeholder
      */
-    mapping(address => uint256) internal stakes;
+    mapping(address => mapping(address => uint256)) internal stakesAmount;
+    mapping(address => mapping(address => uint256)) internal stakesTimestamp;
 
     /**
      * @notice Number of staking slot
      */
     uint slotsAvailable = 100;
 
+    //reason to tell
+    //IERC20 private ERC20Address;
+    //ERC20 private ERC20Address;
+
     /**
      * @notice The accumulated rewards for each stakeholder
      */
     mapping(address => uint256) internal rewards;
 
+    uint256 rate = 5;
+    /**
+     *  @notice list of the events used
+     */
+
+    event StakeholderAdded(address stakeholderAddress);         //admin
+    event StakeholderRemoved(address stakeholderAddress); ////admin
+    event StakeCreated(address stakeholderAddress, uint256 stakeholderStake, address tokenAddress);   //admin & user
+    event StakeRemoved(address stakeholderAddress, uint256 stakeholderStake, address tokenAddress);     //admin & user
+    event RewardCalculated(address stakeholderAddress, address stakeholderReward, address tokenAddress); //admin & user
+    event RewardsDistributed(address stakeholderAddress, address tokenAddress);    //admin & user
+    event RewardWithdrawn(address stakeholderAddress, address tokenAddress);       //admin & user
+
+
+
+    constructor(address _ERC20Address) {
+        require(_ERC20Address != address(0));
+        //ERC20Address = IERC20(_ERC20Address);
+        ERC20Address = ERC20(_ERC20Address);
+    }
+
+
+    /**
+        @notice return the current market value of the locked-in asset
+        @param conversionPriceAddress
+        @return price the current market value
+    */
+    function getPrice(address conversionPriceAddress) public view returns (int) {
+
+        AggregatorV3Interface conversionPrice = AggregatorV3Interface(conversionPriceAddress);
+
+        (uint80 roundID, int256 price, uint256 startedAt, uint256 timeStamp, uint80 answeredInRound) = conversionPrice.latestRoundData();
+
+        return price;
+
+    }
 
     /**
         @notice determines if the address Ethereum is stakeHolder or not
@@ -58,6 +100,8 @@ contract Staking is Ownable {
         (bool _isStakeholder, ) = isStakeholder(_stakeholder);
 
         if(!_isStakeholder) stakeholders.push(_stakeholder);
+
+        emit StakeholderAdded(_stakeholder);
     }
 
     /**
@@ -73,6 +117,9 @@ contract Staking is Ownable {
             stakeholders[s] = stakeholders[stakeholders.length - 1];
             stakeholders.pop();
         }
+
+        emit StakeholderRemoved(_stakeholder);
+
     }
 
     /**
@@ -80,20 +127,20 @@ contract Staking is Ownable {
         @param _addressStakeHolder the address of the stakeHolder to put in order to get the stake
         @return The sum in wei
     */
-    function stakeOf(address _addressStakeHolder) public view returns (uint256){
+    function stakeOf(address _addressStakeHolder, address _tokenAddress) public view returns (uint256,uint256){
 
-        return(stakes[_addressStakeHolder]);
+        return(stakesAmount[_addressStakeHolder][_tokenAddress], stakesTimestamp[_addressStakeHolder][_tokenAddress]);
     }
 
     /**
         @notice aggregates all the stakes of all stakeholders
         @return uint256 The sum of the stakes from all stakeholders
     */
-    function sumOfStakes() public view returns(uint256){
+    function sumOfStakes(address _tokenAddress) public view returns(uint256){
 
         uint256 sumStakes = 0;
         for(uint256 i=0; i < stakeholders.length; i += 1){
-            sumStakes = sumStakes + stakes[stakeholders[i]] ;
+            sumStakes = sumStakes + stakesAmount[stakeholders[i]][_tokenAddress];
         }
 
         return sumStakes;
@@ -103,14 +150,17 @@ contract Staking is Ownable {
         @notice a method for a stakeholder to create a stake
         @param _stake The size of the stake to be created
     */
-    function createStake(uint256 _stake) public {
+    function createStake(uint256 _stake, address _tokenAddress) public {
 
-        if(stakes[msg.sender] == 0){
+        if(stakesAmount[msg.sender] == 0){
             slotsAvailable--;
             addStakeholder(msg.sender);
         }
 
-        stakes[msg.sender] = stakes[msg.sender] + _stake;
+        stakesAmount[msg.sender][_tokenAddress] += _stake;
+        stakesTimestamp[msg.sender][_tokenAddress] = block.timestamp;
+
+        emit StakeCreated(_stake);
     }
 
     /**
@@ -119,12 +169,14 @@ contract Staking is Ownable {
     */
     function removeStake(uint256 _stake) public{
 
-        stakes[msg.sender] = stakes[msg.sender] - _stake;
+        stakesAmount[msg.sender][_tokenAddress] -= _stake;
 
-        if(stakes[msg.sender] == 0){
+        if(stakesAmount[msg.sender][_tokenAddress] == 0){
             removeStakeholder(msg.sender);
             slotsAvailable++;
         }
+
+        emit StakeRemoved(_stake);
     }
 
     /**
@@ -139,7 +191,7 @@ contract Staking is Ownable {
         @notice function to aggregate rewards from all stakeholders
         @return uint256 The aggregated rewards from all stakeholders
     */
-    function totalRewards() public view returns(uint256){
+    function getTotalRewards() public view returns(uint256){
 
         uint256 totalRewards = 0;
 
@@ -155,39 +207,67 @@ contract Staking is Ownable {
         @param _stakeholder The stakeholder to calculate rewards for
         @return uint256 The reward calculated by the formula
     */
-    function calculateReward(address _stakeholder) public view returns(uint256){
+    function calculateReward(address _stakeholder, address _tokenAddress) public view returns(uint256){
 
-        return stakes[_stakeholder] * 60 / 100;
 
+        /*
+        uint now = block.timestamp;
+        //Rate - 5%
+        uint rate = 5;
+        //Dt
+        uint dt = now - stakesTimestamp[_stakeholder][_tokenAddress];
+        //Perodicity - 1 rate / jour
+        uint perio = dt / 1 days;
+        //StakeAmount
+        uint reward = stakesAmount[_stakeholder][_tokenAddress] * rate / 100 * perio;*/
+
+        uint reward = stakesAmount[_stakeholder][_tokenAddress] * 5 / 100 *  ((block.timestamp - stakesTimestamp[_stakeholder][_tokenAddress]) / 1 days);
+
+
+        emit RewardCalculated(_stakeholder, reward);
+        return reward;
     }
 
     /**
         @notice function to distribute rewards to all stakeholders
     */
-    function distributeRewards() public onlyOwner {
+    function distributeRewards(address _tokenAddress) public onlyOwner {
 
         for (uint256 i = 0; i < stakeholders.length; i++ ){
             address stakeholder = stakeholders[i];
-            uint256 reward = calculateReward(stakeholder);
+            uint256 reward = calculateReward(stakeholder, _tokenAddress);
             rewards[stakeholder] = rewards[stakeholder] + reward;
         }
+
+        emit RewardsDistributed();
+    }
+
+    /**
+        @notice function for user to claim rewards
+    */
+    function claimRewards(address _tokenAddress) public onlyStakeholder {
+
+        uint256 reward = calculateReward(msg.sender, _tokenAddress);
+        rewards[stakeholder] = rewards[msg.sender] + reward;
+
     }
 
     /**
         @notice function to allow the stakeholder to withdraw his rewards
     */
     function withdrawReward() public {
+        require(rewards[msg.sender] == 0);
 
         uint256 reward = rewards[msg.sender];
-        //maybe the stakeholder will withdraw all his reward in one blow
-        if(rewards[msg.sender] == 0){
-            slotsAvailable++;
-        }
+        //maybe the stakeholder will withdraw all his reward in one blow. Evidence: no argument in the signature of
+        //the function
 
-        //rewards[msg.sender] = 0;
-        //_mint(msg.sender, reward);  NO!
+        //ERC20Address._mint(msg.sender, reward);
 
+        //to put the rewards at 0
+        rewards[msg.sender] = 0;
 
+        emit RewardWithdrawn();
     }
 }
 
--- test/staking.test.js
+++ test/staking.test.js
@@ -1,19 +1,81 @@
 const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
 const Staking = artifacts.require('Staking');
+const StakeCoin = artifacts.require('StakeCoin');
 
 contract("Staking", accounts => {
 
     const owner = accounts[0];
+    const stakeholder1 = accounts[1];
+    const stakeholder2 = accounts[2];
+    const amount1 = new BN(10);
+    const amount2 = new BN(20);
 
     /**
      * Clean each time the instance of the smart contract
      */
     beforeEach(async function () {
 
-        this.StakingInstance = await Staking.new({from: owner});
+        this.stakingInstance = await Staking.new({from: owner});
+        this.stakeCoinInstance = await StakeCoin.new({from: owner});
     });
 
+
+    /**
+     *
+     */
+    describe('A. Tests of the different functions', function() {
+
+        it('1. stake creation test', async function() {
+            // transferer des tokens à stakeholder1
+            await this.stakeCoinInstance.transfer(stakeholder1, amount1, {from:owner});
+
+            // stakeholder1 stake (on teste l'event)
+            expectEvent(await this.stakingInstance.createStake(amount1, {from: stakeholder1}),
+                'StakeCreated', {stakeholderStake: amount1 });
+
+            // on utilise la fonction balanceOf de stakeCoinInstance pour vérifier que stakeholder1 a bien reçu
+            // les tokens et qui correspond à amount1
+            const balance = await this.stakeCoinInstance.balanceOf(stakeholder1);
+            expect(balance).to.equal(amount1);
+
+            // vérifier le montant staké (stakeOf)
+        })
+
+    });
+
+    /**
+     *  Management of the different events of the staking smart contract
+     */
+    describe('B. Tests of the events of the staking smart contract', function() {
+
+        //need to specify {from: owner}?
+        it('1. Sends an event after to have added a stakeholder', async function() {
+            expectEvent(await this.stakingInstance.addStakeholder(stakeholder1, {from: owner}),
+                'StakeholderAdded', {stakeholderAddress: stakeholder1 });
+        });
+
+        //need to specify {from: owner}?
+        it('2. Sends an event after to have removed a stakeholder ', async function() {
+            expectEvent( await this.stakingInstance.removeStakeholder(stakeholder1, {from:owner}),
+                'StakeholderRemoved', {stakeholderAddress: stakeholder1});
+        });
+
+        it('3. Sends an event after the stake was created', async function() {
+            expectEvent( await this.stakingInstance.createStake(amount2, {from:stakeholder2}),
+                'StakeCreated', {stakeholderStake: amount2 });
+        });
+
+        it('4. Sends an event after the stake was removed', async function() {
+            expectEvent( await this.stakingInstance.removeStake(amount2, {from:stakeholder2}),
+                'StakeRemoved', {stakeholderStake: amount2});
+        })
+
+    });
+
+
+
+    /*
     describe('Yield farming', async() => {
 
         it('Reverts if the amount given is equal to zero', async function() {
@@ -23,6 +85,6 @@ contract("Staking", accounts => {
 
         })
     });
-
+    */
 
 })
