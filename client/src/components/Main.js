d507ff3f996e6c9bd6d27d54484c33b0369ab33c
Alex4422 <alexandregautier82@gmail.com>
2021-12-02 17:34:54 +0100

    new component for the GUI

---
+++ client/src/components/Main.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+
+    render() {
+        return (
+
+            // <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div id='content' className='mt-3' >
+                <table className='table text-muted text-center'>
+                    <thead>
+                        <tr style={{color:'white'}}>
+                            <th scope='col'>Staking Balance</th>
+                            <th scope='col'>Reward Balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr style={{color:'black'}}>
+                          <td>Token</td>
+                          <td>Reward associated</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div className='card mb-2' style={{opacity:'.9'}}>
+                    <form className='mb-3'>
+                        <div style={{borderSpacing:'0 1em'}}>
+                            <label className='float-left' style={{marginLeft:'15px'}}><b>Stake tokens</b></label>
+                            <span className='float-right' style={{marginRight:'8px'}}>
+                                Balance:
+                            </span>
+
+                            <div className='input-group mb-4'>
+                                <input
+                                    type='text'
+                                    placeholder='0'
+                                    required
+                                />
+                            </div>
+                                <button type='submit' className='btn btn-primary btn-lg btn-block'  >DEPOSIT</button>
+                        </div>
+                    </form>
+                    <button className='btn btn-primary btn-lg btn-block' style={{maxWidth:'600px'}}>WITHDRAW</button>
+
+                    <div className='card-body text-center' style={{color:'blue'}}>
+                        AIRDROP
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Main;
